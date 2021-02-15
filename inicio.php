ca8dfacb23763da5fc2cb8c52229dc5a2f2ce584
ferny96 <fernyrodriguez97@gmail.com>
2021-02-15 17:53:10 -0600

    Creando el archivo inicio.php

---
+++ inicio.php
@@ -0,0 +1,12 @@
+<?php
+    class inicio{
+
+        public function ctrInicio(){
+
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Accusantium debitis sint in repellendus alias corporis officia beatae?
+            Veniam sunt, animi dolorum pariatur qui culpa repellendus a,
+            illo aspernatur, eos consequatur?
+        }
+    }
+?>
